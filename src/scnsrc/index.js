44b84fada7d9f2e720ac62dbb1ba3d1b4cdd2b70
prashant <prashantkamdar@gmail.com>
2020-04-27 18:29:26 +0530

    scnsrc callback

--- src/scnsrc/index.js
+++ src/scnsrc/index.js
@@ -5,43 +5,50 @@ posts = [];
 
 url = 'https://www.scnsrc.me/page/1/';
 
-function getPosts(){
+let headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0',
+    'Cookie': '__cfduid=d3f1c963948cd66bab6de1c6e0ec8e4141587982689; cf_clearance=c9d0462d0720ac241daaf5e778bceb9ea9ba532d-1587982706-0-150; MintAcceptsCookies=1; MintUnique=1; MintUniqueDay=1587963600; MintUniqueWeek=1587877200; MintUniqueMonth=1585717200; MintUniqueLocation=1; __cf_bm=8c9ef3b483f2543a77de506d51094626ffd5288d-1587987784-1800-AYlY8TpPBhTTJfz6Z84bOejRFTWSwJa1AZiGcuJCpcWMY3vBQhgTEKcZ3qjBySf+dHwDsGciwUSdnLpHUyYcig6DMB5tmt++4O8kbPwdsEff; MintUniqueHour=1587985200'
+};
 
-request(url, function(error, response, html) {
-    if (!error && response.statusCode == 200) {
-        
-        let $ = cheerio.load(html);
+let options = {
+    url: url,
+    headers: headers
+};
 
-        $("div[class='post']").each(function(i,post){
-            
-            name = $("h2 a", post).text();
-            //console.log(name);
+let getPosts = function(callback)  {
+    request(options, function(error, response, html) {
+        if (!error && response.statusCode == 200) {
 
-            tags = $("a[rel='category tag']", post).toArray().map(element => $(element).text());
-            //console.log(tags);
+            let $ = cheerio.load(html);
 
-            titles = $("p > strong", post)
-            .filter(function(){
-                return $(this).text().indexOf(':') === -1;
-            })
-            .toArray()
-            .map(element => $(element).text());
+            $("div[class='post']").each(function(i, post) {
 
-            //console.log(titles);
+                name = $("h2 a", post).text();
+                //console.log(name);
 
-            posts.push({"name":name, "tags":tags, "titles":titles});
+                tags = $("a[rel='category tag']", post).toArray().map(element => $(element).text());
+                //console.log(tags);
 
+                titles = $("p > strong", post)
+                    .filter(function() {
+                        return $(this).text().indexOf(':') === -1;
+                    })
+                    .toArray()
+                    .map(element => $(element).text());
 
-            //console.log("+-+-+-+-+-+-++-+-")
-        });
+                posts.push({
+                    "name": name,
+                    "tags": tags,
+                    "titles": titles
+                });
+            });
 
-        return posts;
+            callback(null, posts); 
 
-
-    } else {
-        console.log(error);
-    }
-});
+        } else {
+            console.log("Error: " + error);
+        }
+    });
 }
 
-exports.getPosts = getPosts;
+module.exports.getPosts = getPosts;
